18575f54bdecfa44e9021542a6bad34a8d2f2bb1
Víctor J <maximovj@outlook.com>
2024-09-09 14:55:15 -0600

    * Se corrige middleware para JSON Web Token

--- express-api/src/config/configJwt.js
+++ express-api/src/config/configJwt.js
@@ -5,7 +5,7 @@ dotenv.config();
 const secret_key = process.env.APP_SECRET_KEY || 'Yx>qor]QhQzjxp+13`QKB~@nc~CRRw';
 
 const configJwt = (req, res, next) => {
-    const auth = req.headers['authorization'] || null;
+    const auth = req.headers['authorization'] || '';
     const bearer_token = auth.split(' ');
 
     if (auth && bearer_token[0].indexOf('Bearer') === 0 && bearer_token[1]) {
@@ -20,6 +20,12 @@ const configJwt = (req, res, next) => {
             }
             req.session_payload = payload;
             next();
+        });
+    } else {
+        return res.status(403).json({
+            ctx_content: 'Token no proporcionado.',
+            success: false,
+            source: null,
         });
     }
 };
