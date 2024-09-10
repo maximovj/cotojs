5901cd6c91f841dd3bfc4433959f202aca95daaf
Víctor J <maximovj@outlook.com>
2024-09-09 19:30:32 -0600

    * Se configura cookies para JWT

--- express-api/src/config/configJwt.js
+++ express-api/src/config/configJwt.js
@@ -5,29 +5,15 @@ dotenv.config();
 const secret_key = process.env.APP_SECRET_KEY || 'Yx>qor]QhQzjxp+13`QKB~@nc~CRRw';
 
 const configJwt = (req, res, next) => {
-    const auth = req.headers['authorization'] || '';
-    const bearer_token = auth.split(' ');
+    const token = req.cookies.auth_token;
 
-    if (auth && bearer_token[0].indexOf('Bearer') === 0 && bearer_token[1]) {
-        const token = bearer_token[1];
-        jwt.verify(token, secret_key, (err, payload) => {
-            if (err) {
-                return res.status(401).json({
-                    ctx_content: 'Acceso denegado.',
-                    success: false,
-                    source: null,
-                });
-            }
-            req.session_payload = payload;
-            next();
-        });
-    } else {
-        return res.status(403).json({
-            ctx_content: 'Token no proporcionado.',
-            success: false,
-            source: null,
-        });
-    }
+    if (!token) return res.status(401).json({ ctx_content: 'Acceso no autorizado.', success: false });
+
+    jwt.verify(token, secret_key, (err, payload) => {
+        if (err) return res.status(403).json({ ctx_content: 'Token inválido.', success: false });
+        req.session_payload = payload;
+        next();
+    });
 };
 
 export default configJwt;
