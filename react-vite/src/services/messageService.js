ace7690cad78a8dfaee0c897d60264a00aa4a8ad
Víctor J <maximovj@outlook.com>
2024-09-14 01:07:20 -0600

    * Se agrega servicio de mensajes en FrontEnd con ReactJS + Vite

---
+++ react-vite/src/services/messageService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const baseURL = import.meta.env.VITE_API_URL;
+
+const messageService = axios.create({
+    baseURL: `${baseURL}/message`,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    withCredentials: true,
+});
+
+export function messageServiceCreate(id, data) {
+    return messageService.post(`/room/${id}`, data);
+}
+
+export function messageServiceFind(id) {
+    return messageService.get(`/room/${id}`);
+}
