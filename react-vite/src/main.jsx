c77e3d706c56f586ed268b4cdd476d796238bdce
Víctor J <maximovj@outlook.com>
2024-09-14 10:54:21 -0600

    * Se agrega socket.io-client para la conexión a socket desde el FrontEnd

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './services/socketService.js';
 
 createRoot(document.getElementById('root')).render(
   <App />
--- react-vite/src/services/socketService.js
+++ react-vite/src/services/socketService.js
@@ -1,11 +1,34 @@
-import io from 'socket.io-client';
+import { io } from "socket.io-client";
+const domain = import.meta.env.VITE_SOCKET_IO_DOMAIN;
+const path = import.meta.env.VITE_SOCKET_IO_PATH;
 
-const socketService = io('http://localhost:3099', {
-    host: 'http://localhost',
-    port: 3099,
-    path: '/api/v1/socket.io',
+const socketService = io(`${domain}`, {
+    path: `${path}`,
     transports: ['websocket'],
     withCredentials: true,
+});
+
+socketService.on("connect", () => {
+    socketService.emit("presentacion_bienvenida", "Hola me conecto, este es mi id: " + socketService.id);
+    //console.log('ha establecido conexión con socket.io');
+});
+
+socketService.on('presentacion_respuesta', (arg) => {
+    console.log(arg);
+});
+
+socketService.on("disconnect", () => {
+    console.log('ha perdido conexión con socket.io');
+});
+
+socketService.on("connect_error", (error) => {
+    if (socketService.active) {
+        // temporary failure, the socket will automatically try to reconnect
+    } else {
+        // the connection was denied by the server
+        // in that case, `socket.connect()` must be manually called in order to reconnect
+        console.log(error.message);
+    }
 });
 
 export default socketService;
