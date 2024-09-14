821f27d193c567273cd68c44ec9ca3b6f38530e4
Víctor J <maximovj@outlook.com>
2024-09-14 17:12:37 -0600

    * Se reemplaza roomServiceList a roomServiceAll

--- react-vite/src/services/roomService.js
+++ react-vite/src/services/roomService.js
@@ -14,8 +14,10 @@ export function roomServiceCreate(data) {
     return roomService.post('/', data);
 }
 
-export function roomServiceList() {
-    return roomService.get('/all');
+export function roomServiceAll(page = 1, limit = 10) {
+    return roomService.get('/all', {
+        params: { page, limit }
+    });
 }
 
 export function roomServiceFind(id) {
