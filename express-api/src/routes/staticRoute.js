f224e3bf56d6a2d8bbeff9740b75eeff02f3d0ab
Víctor J <maximovj@outlook.com>
2024-09-17 11:41:13 -0600

    * Se agrega sharp para manipular imágenes. En lado, BackEnd.

--- express-api/src/routes/staticRoute.js
+++ express-api/src/routes/staticRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fs from 'fs';
+import sharp from 'sharp';
 import path from 'path';
 import multer from 'multer';
 import room from '../models/roomModel.js';
@@ -9,6 +10,32 @@ import user from '../models/userModel.js';
 const staticRoute = express.Router();
 const Room = room;
 const User = user;
+
+// Obtén la ruta del directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Define el directorio donde están los archivos estáticos
+const staticDir = path.join(__dirname, '../../storage');
+
+async function retryDelete(filePath, retries = 3, delay = 500) {
+    while (retries > 0) {
+        try {
+            await fs.unlink(filePath);
+            console.log(`Archivo eliminado: ${filePath}`);
+            return;
+        } catch (err) {
+            if (err.code === 'EPERM' || err.code === 'EBUSY') {
+                console.warn(`Error al eliminar el archivo, reintentando... (${retries})`);
+                retries -= 1;
+                await new Promise(resolve => setTimeout(resolve, delay)); // Espera antes de reintentar
+            } else {
+                throw err;
+            }
+        }
+    }
+    console.error(`No se pudo eliminar el archivo después de varios intentos: ${filePath}`);
+}
 
 const storage_room_cover = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -50,28 +77,45 @@ staticRoute.post('/rooms/cover/:id', upload_room_cover.single('cover'), async (r
     }
 });
 
+// Configurar multer para almacenar las imágenes
 const upload_users_picture = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, 'storage/users/picture');
+            cb(null, 'upload/users/picture'); // Carpeta donde se guardarán las imágenes
         },
         filename: (req, file, cb) => {
-            cb(null, Date.now() + path.extname(file.originalname));
-        }
-    })
+            cb(null, Date.now() + path.extname(file.originalname)); // Asignar nombre único
+        },
+    }),
 });
 
+// Ruta para subir la imagen de perfil
 staticRoute.post('/users/picture/:id', upload_users_picture.single('picture'), async (req, res) => {
     try {
         const user_id = req.params.id;
-        const pathPicture = req.file.path;
+        const originalPath = req.file.path; // Ruta original de la imagen
 
         const find_user = await User.findById(user_id);
 
         if (find_user) {
-            find_user.picture = pathPicture || find_user.picture;
-            // Actualizar la propiedad `picture`
+            // Generar una nueva ruta para el thumbnail (nombre diferente)
+            const thumbnail = path.join(`storage`, `users`, `thumbnail`, `thumbnail_${Date.now()}.jpg`);
+
+            // Usar Sharp para reducir el tamaño de la imagen, crear y guardar el thumbnail 
+            await sharp(originalPath)
+                .resize(300, 300)
+                .jpeg({ quality: 80 })
+                .toFile(thumbnail);
+
+            // Eliminar la imagen original si ya no es necesaria
+            //fs.rmSync(originalPath);
+
+            // Actualizar el campo `picture` del usuario con la ruta del thumbnail
+            find_user.picture = thumbnail || find_user.picture;
+
+            // Guardar los cambios en la base de datos
             await find_user.updateOne(find_user._doc);
+
             return res.status(200).json({
                 ctx_content: 'Foto de perfil actualizada con éxito',
                 success: true,
@@ -92,13 +136,6 @@ staticRoute.post('/users/picture/:id', upload_users_picture.single('picture'), a
         });
     }
 });
-
-// Obtén la ruta del directorio actual
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Define el directorio donde están los archivos estáticos
-const staticDir = path.join(__dirname, '../../static');
 
 staticRoute.get('/rooms/cover/:filename', (req, res) => {
     const filename = req.params.filename;
