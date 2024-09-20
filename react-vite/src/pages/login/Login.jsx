277bae23858fc9acfa300af0f79a51e7e11b6812
Víctor J <maximovj@outlook.com>
2024-09-19 22:33:43 -0600

    * Se agrega funcionalidad de iniciar sesión. En lado, FrontEnd.

--- react-vite/src/pages/login/Login.jsx
+++ react-vite/src/pages/login/Login.jsx
@@ -1,21 +1,62 @@
+import { useState } from 'react';
+import { useSweetAlert } from '../../hooks/useSweetAlert';
+import { useToast } from '../../hooks/useToast';
+import { useAuth } from '../../hooks/useAuth';
 import '../../bubbles.css';
 import default_picture_user from '../../assets/usuario_256.png';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login } = useAuth();
+    const { showSweetAlert } = useSweetAlert();
+    const showToast = useToast();
+
+    const handleInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        if (name === 'email') {
+            setEmail(value);
+        } else
+            if (name === 'password') {
+                setPassword(value);
+            }
+    }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email || !password) {
+            showSweetAlert({
+                icon: 'info',
+                title: 'Iniciar sesión',
+                html: 'Todos los campos son obligatorios.',
+                showConfirmButton: true,
+            });
+            return;
+        }
+
+        setEmail('');
+        setPassword('');
+        login({ email, password });
+    }
+
+
     return (
         <div className="relative flex flex-col items-center justify-center min-h-screen p-6 space-y-12">
             {/* Fondo de Burbujas */}
             <div className="bubbles">
-                <div className="bubble bubble1" style={{left: '20%', top: '20%'}}></div>
-                <div className="bubble bubble2" style={{left: '80%', top: '60%'}}></div>
-                <div className="bubble bubble3" style={{left: '15%', top: '75%'}}></div>
-                <div className="bubble bubble4" style={{left: '80%', top: '20%'}}></div>
-                <div className="bubble bubble5" style={{left: '75%', top: '75%'}}></div>
-                <div className="bubble bubble6" style={{left: '10%', top: '50%'}}></div>
-                <div className="bubble bubble7" style={{left: '10%', top: '35%'}}></div>
-                <div className="bubble bubble8" style={{left: '70%', top: '35%'}}></div>
-                <div className="bubble bubble9" style={{left: '83%', top: '80%'}}></div>
-                <div className="bubble bubble10" style={{left: '13%', top: '65%'}}></div>
+                <div className="bubble bubble1" style={{ left: '20%', top: '20%' }}></div>
+                <div className="bubble bubble2" style={{ left: '80%', top: '60%' }}></div>
+                <div className="bubble bubble3" style={{ left: '15%', top: '75%' }}></div>
+                <div className="bubble bubble4" style={{ left: '80%', top: '20%' }}></div>
+                <div className="bubble bubble5" style={{ left: '75%', top: '75%' }}></div>
+                <div className="bubble bubble6" style={{ left: '10%', top: '50%' }}></div>
+                <div className="bubble bubble7" style={{ left: '10%', top: '35%' }}></div>
+                <div className="bubble bubble8" style={{ left: '70%', top: '35%' }}></div>
+                <div className="bubble bubble9" style={{ left: '83%', top: '80%' }}></div>
+                <div className="bubble bubble10" style={{ left: '13%', top: '65%' }}></div>
             </div>
 
             {/* Sección Visual */}
@@ -33,16 +74,22 @@ const Login = () => {
                 {/* Formulario de Inicio de Sesión */}
                 <div className="bg-white p-8 max-w-lg w-full shadow-md rounded-lg">
                     <h2 className="text-2xl font-bold text-gray-800 mb-4">Iniciar sesión</h2>
-                    <form className="flex flex-col space-y-4">
+                    <form onSubmit={handleOnSubmit} className="flex flex-col space-y-4">
                         <input
                             type="email"
                             placeholder="Correo electrónico"
+                            value={email}
+                            name="email"
+                            onChange={handleInput}
                             className="border border-gray-300 rounded-md p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             required
                         />
                         <input
                             type="password"
                             placeholder="Contraseña"
+                            name="password"
+                            value={password}
+                            onChange={handleInput}
                             className="border border-gray-300 rounded-md p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             required
                         />
